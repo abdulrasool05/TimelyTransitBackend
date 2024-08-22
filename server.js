2575a5c01f824c68693a529a23d3d89801adca6f
Abdul Rasool <146143358+abdulrasool05@users.noreply.github.com>
2024-08-22 01:36:37 -0400

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const app = express();
 const cors = require("cors")
 const corsOptions = {
-    origin: ["http://localhost:5173"],
+    origin: ["https://timelytransitclient.onrender.com/"],
 };
 
 app.use(cors(corsOptions));

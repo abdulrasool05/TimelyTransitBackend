12011f543fb04f4b4a357b6a9d9009bbee25e069
Abdul Rasool <146143358+abdulrasool05@users.noreply.github.com>
2024-09-07 01:02:37 -0400

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const cors = require("cors")
 
 const corsOptions = {
-    origin: 'https://timelytransitclient.onrender.com/',  
+    origin: 'https://timelytransitclient.onrender.com',  
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',           
     credentials: true,                                  
     allowedHeaders: 'Content-Type,Authorization'         
